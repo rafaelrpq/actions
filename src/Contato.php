53828c37d7751e59cf8a4ea41813ff946a5b83de
Rafael Quevedo <rafaelrpq@gmail.com>
2023-06-06 13:38:01 -0300

    inclusão de erro e código repetido

--- src/Contato.php
+++ src/Contato.php
@@ -2,17 +2,43 @@
     class Contato {
         public function __construct (private $nome, private $endereco, private $telefone, private $email) {}
 
-        public function getNome () {
+        public function getNome () : string {
             return $this->nome;
         }
-        public function getEndereco () {
+        public function getEndereco () : string {
             return $this->endereco;
         }
-        public function getTelefone () {
+        public function getTelefone () : string {
             return $this->telefone;
         }
-        public function getEmail () {
-            // return $this->email;
+        public function getEmail () : string {
+            return $this->email;
+        }
+
+        public function codigo () {
+            if (strpos ($this->email, '@') === false) {
+                print ('email invalido');
+                exit (1);
+            }
+
+
+            print ('código asfasdfasdf');
+            /*
+                codigo a ser executado
+            */
+        }
+
+        public function codigoRepetido () {
+            if (strpos ($this->email, '@') === false) {
+                print ('email invalido');
+                exit (1);
+            }
+
+            print ('código 239r8-123rmj0-238r0123b0r');
+
+            /*
+                codigo a ser executado
+            */
         }
     }
 ?>
