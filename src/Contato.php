66881537fee96049079c51795d9856a346184a7f
Rafael Quevedo <rafaelrpq@gmail.com>
2023-06-06 14:29:38 -0300

    phpmd teste

--- src/Contato.php
+++ src/Contato.php
@@ -1,6 +1,6 @@
 <?php
     class Contato {
-        public function __construct (private $nome, private $endereco, private $telefone, private $email) {}
+        public function __construct (private string $nome, private string $endereco, private string $telefone, private string $email) {}
 
         public function getNome () : string {
             return $this->nome;
@@ -11,11 +11,11 @@
         public function getTelefone () : string {
             return $this->telefone;
         }
-        public function getEmail () : string {
-            return $this->email;
-        }
+        // public function getEmail () : string {
+        //     return $this->email;
+        // }
 
-        public function codigo () {
+        public function codigo () : void {
             if (strpos ($this->email, '@') === false) {
                 print ('email invalido');
                 exit (1);
@@ -28,7 +28,7 @@
             */
         }
 
-        public function codigoRepetido () {
+        public function codigoRepetido () : void {
             if (strpos ($this->email, '@') === false) {
                 print ('email invalido');
                 exit (1);
@@ -40,5 +40,154 @@
                 codigo a ser executado
             */
         }
+
+
+        public function example1 () {
+            if ($a == $b) {
+                if ($a1 == $b1) {
+                    fiddle();
+                } elseif ($a2 == $b2) {
+                    fiddle();
+                } else {
+                    fiddle();
+                }
+            } elseif ($c == $d) {
+                while ($c == $d) {
+                    fiddle();
+                }
+            } elseif ($e == $f) {
+                for ($n = 0; $n < $h; $n++) {
+                    fiddle();
+                }
+            } else {
+                switch ($z) {
+                    case 1:
+                        fiddle();
+                        break;
+                    case 2:
+                        fiddle();
+                        break;
+                    case 3:
+                        fiddle();
+                        break;
+                    default:
+                        fiddle();
+                        break;
+                }
+            }
+        }
+
+        public function example2 () {
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+            print ('código 239r8-123rmj0-238r0123b0r');
+        }
     }
+
+
+    $novo = new Contato ('teste', 'av 12 de agosto, n 123', '55-98765 4321', '');
 ?>
