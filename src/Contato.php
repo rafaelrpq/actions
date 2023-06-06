5503176dd002d0c07505f61df97d8ce6f5844428
Rafael Quevedo <rafaelrpq@gmail.com>
2023-06-06 13:21:28 -0300

    inclusão do phpstan

---
+++ src/Contato.php
@@ -0,0 +1,18 @@
+<?php
+    class Contato {
+        public function __construct (private $nome, private $endereco, private $telefone, private $email) {}
+
+        public function getNome () {
+            return $this->nome;
+        }
+        public function getEndereco () {
+            return $this->endereco;
+        }
+        public function getTelefone () {
+            return $this->telefone;
+        }
+        public function getEmail () {
+            return $this->email;
+        }
+    }
+?>
