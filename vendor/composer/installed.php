15017db0a309bbc56a8b8dba2f26dcf6e5985a6f
Rafael Quevedo <rafaelrpq@gmail.com>
2023-06-06 12:59:14 -0300

    'adicionando composer, phpstan'

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a1194794c87f495de6a6a3fded0cff4ea2f4a05f',
+        'name' => 'rafaelrpq/actions',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.10.16',
+            'version' => '1.10.16.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'reference' => '352bdbb960bb523e3d71b834862589f910921c23',
+            'dev_requirement' => false,
+        ),
+        'rafaelrpq/actions' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a1194794c87f495de6a6a3fded0cff4ea2f4a05f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
