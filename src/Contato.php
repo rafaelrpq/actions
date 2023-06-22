37bb02b547ef755386ef75a2c002438ba46711bb
Rafael Quevedo <rafaelrpq@gmail.com>
2023-06-22 12:39:48 -0300

    atualizando contato

--- src/Contato.php
+++ src/Contato.php
@@ -4,7 +4,7 @@
          private string $endereco;
          private string $telefone;
          private string $email;
- 
+
          private function __construct(string $nome, string $endereco, string $telefone, string $email){
              $this->nome = $nome;
              $this->endereco = $endereco;
@@ -18,30 +18,30 @@
              }
              return new self($contato['nome'], $contato['endereco'], $contato['telefone'], $contato['email']);
          }
- 
+
          public function getNome(): string {
              return $this->nome;
          }
- 
+
          public function getEndereco(): string {
              return $this->endereco;
          }
- 
+
          public function getTelefone(): string {
              return $this->telefone;
          }
- 
+
          public function getEmail(): string {
              return $this->email;
          }
- 
+
          public function codigo() : void {
              if (!strpos ($this->email, '@')){
                 throw new InvalidArgumentException('Email inválido');
              }
              //codigo a ser executado
          }
- 
+
          public function codigoRepetido () : void {
              if (!strpos ($this->email, '@')){
                 throw new InvalidArgumentException('Email inválido');
@@ -52,7 +52,7 @@
              }
              //codigo a ser executado
          }
- 
+
          public function example2(): void {
             echo "código 239r8-123rmj0-238r0123b0r {$this->getNome()}";
              if (!strpos($this->email, '@')){
