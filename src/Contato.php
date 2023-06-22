99b04cc0db1dc87a5b9f4fd3624214b1ad8c33b8
Rafael Quevedo <rafaelrpq@gmail.com>
2023-06-22 12:42:51 -0300

    atualizando contato

--- src/Contato.php
+++ src/Contato.php
@@ -1,62 +1,76 @@
- <?php
-     class Contato {
-         private string $nome;
-         private string $endereco;
-         private string $telefone;
-         private string $email;
+<?php
+    class Contato {
+        // public function __construct (private string $nome, private string $endereco, private string $telefone, private string $email) {}
 
-         private function __construct(string $nome, string $endereco, string $telefone, string $email){
-             $this->nome = $nome;
-             $this->endereco = $endereco;
-             $this->telefone = $telefone;
-             $this->email = $email;
-         }
+        private string $nome;
+        private string $endereco;
+        private string $telefone;
+        private string $email;
 
-         public static function fromArray(array $contato): Contato{
-             if (!isset($contato['nome'], $contato['endereco'], $contato['telefone'], $contato['email'])){
-                throw new InvalidArgumentException("Contato array must have keys 'nome', 'endereco', 'telefone' and 'email'");
-             }
-             return new self($contato['nome'], $contato['endereco'], $contato['telefone'], $contato['email']);
-         }
+        /**
+         * @param array<string> $contato
+         */
+        public function __construct (array $contato = []) {
+            $this->nome = $contato['nome'];
+            $this->endereco = $contato['endereco'];
+            $this->telefone = $contato['telefone'];
+            $this->email = $contato['email'];
+        }
 
-         public function getNome(): string {
-             return $this->nome;
-         }
+        public function getNome () : string {
+            return $this->nome;
+        }
 
-         public function getEndereco(): string {
-             return $this->endereco;
-         }
+        public function getEndereco () : string {
 
-         public function getTelefone(): string {
-             return $this->telefone;
-         }
+            return $this->endereco;
+        }
+        public function getTelefone () : string {
+            return $this->telefone;
+        }
+        public function getEmail () : string {
+            return $this->email;
+        }
 
-         public function getEmail(): string {
-             return $this->email;
-         }
+        public function codigo () : void {
+            if (strpos ($this->email, '@') === false) {
+                print ('email invalido');
+                exit (1);
+            }
+            /*
+                codigo a ser executado
+            */
+        }
 
-         public function codigo() : void {
-             if (!strpos ($this->email, '@')){
-                throw new InvalidArgumentException('Email inválido');
-             }
-             //codigo a ser executado
-         }
+        public function codigoRepetido () : void {
+            if (strpos ($this->email, '@') === false) {
+                print ('email invalido');
+                exit (1);
+            }
 
-         public function codigoRepetido () : void {
-             if (!strpos ($this->email, '@')){
-                throw new InvalidArgumentException('Email inválido');
-             }
-             echo ('código 239r8-123rmj0-238r0123b0r'. $this->getTelefone());
-             if (strlen($this->getNome()) > 0){
-                 echo "teste: ".$this->getNome();
-             }
-             //codigo a ser executado
-         }
+            print ('código 239r8-123rmj0-238r0123b0r'. $this->getTelefone());
 
-         public function example2(): void {
-            echo "código 239r8-123rmj0-238r0123b0r {$this->getNome()}";
-             if (!strpos($this->email, '@')){
-                throw new InvalidArgumentException('Email inválido');
-             }
-         }
-     }
+            if (strlen ($this->getNome()) > 0) {
+                echo "teste: $this->getNome()";
+            }
+            /*
+                codigo a ser executado
+            */
+        }
+
+        public function example2 () : void {
+            print ('código 239r8-123rmj0-238r0123b0r {$this->getNome()}');
+        }
+
+        public static function Teste () : void {
+            echo "metodo estatico";
+        }
+    }
+
+
+    $novo = new Contato ([
+        'nome' => 'teste',
+        'endereco' => 'av 12 de agosto, n 123',
+        'telefone' => '55-98765 4321',
+    ]);
+?>
